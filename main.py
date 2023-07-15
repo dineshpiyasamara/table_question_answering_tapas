add4828ad386b1d0ffdda34280ed399315fb983e
Dinesh <silvershine.zero@gmail.com>
2023-07-15 09:34:05 +0530

    add comments

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# import necessary libraries
 import tkinter as tk
 from tkinter import ttk
 from tkinter.font import Font
 from transformers import pipeline
 import pandas as pd
 
+# initialize tkinter window
 root = tk.Tk()
 root.title("Table Q&A")
 root.geometry("500x400")
 root.iconbitmap('icon.ico')
 
+# create model object
 get_answer = pipeline(task='table-question-answering', model='google/tapas-base-finetuned-wtq')
 
+# styling
 style = ttk.Style()
 style.configure("Treeview.Heading", font=("TkDefaultFont", 12, "bold"))
 style.configure("Treeview", font=("TkDefaultFont", 12))
@@ -22,12 +26,14 @@ data = {
     'results':['Pass', 'Fail', 'Fail', 'Pass', 'Pass']
 }
 
+# get model prediction
 def handle_enter(event):
     df = pd.DataFrame(data)
     model_answer = get_answer(table=df.astype(str), query=question.get())
     answer.configure(text=model_answer['answer'])
     question.set('')
 
+# initialize table view
 table = ttk.Treeview(root, columns=('name', 'age', 'result'), show='headings')
 table.column('age', width=150, anchor='center')
 table.column('result', width=150, anchor='center')
@@ -37,12 +43,14 @@ table.heading('age', text='Age')
 table.heading('result', text='Result')
 table.pack(pady=(10,0))
 
+# insert data to table
 for idx, value in enumerate(data['names']):
     table.insert('', index=idx, values=(data['names'][idx], data['ages'][idx], data['results'][idx]))
 
 queston_label = ttk.Label(root, text='Question', font=Font(weight='bold'))
 queston_label.pack(pady=(10,0))
 
+# tkinter variable to store question
 question = tk.StringVar(value = '')
 
 question_entry = ttk.Entry(root, textvariable=question, font=("TkDefaultFont", 12), width=50)
@@ -54,6 +62,8 @@ answer_label.pack(pady=(10,0))
 answer = ttk.Label(root, text='', font=("TkDefaultFont", 12))
 answer.pack()
 
+# event binding
 root.bind("<Return>", handle_enter)
 
+# run the window
 root.mainloop()
